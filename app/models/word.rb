55406a0e60bbcc8b83dda72fe6237c4443e34465
Chris Camp & Thierry Moudiki <pair+cpcamp2.thierrymoudiki@devbootcamp.com>
2017-09-19 19:20:14 -0400

    finish release 2 - find anagrams

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,12 @@
+WORD_LIST_FILENAME = "db/fixtures/abridged_word_list.txt"
+
 class Word < ApplicationRecord
   def anagrams
-    hsh = {}
-    File.foreach(WORD_LIST_FILENAME) do |line|
-    # write code to create a new record in the database for each line of the input file
-      hsh[line.chars.sort.join.downcase] = Word.create(word: line)
+    hsh_words = {}
+    File.foreach(WORD_LIST_FILENAME) do |word|
+      word = word.gsub("\n","")
+      hsh_words[word] = word.chars.sort.join.downcase
     end
+    return hsh_words.select{|k, v| v == self.word.chars.sort.join.downcase}.map{|k, v| k}
   end
 end
