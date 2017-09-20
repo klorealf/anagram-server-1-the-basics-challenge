8bef904e1e235ee9c271c35ab4b76c685880e5c4
Andres Arias & Samory Harris <pair+aarias89.samory070@devbootcamp.com>
2017-09-20 08:55:19 -0400

    Initial commit

---
+++ app/models/word.rb
@@ -0,0 +1,8 @@
+class Word < ApplicationRecord
+  # def anagrams(file)
+  #   word_collection = []
+  #   word_collection.each do |i|
+  #     i.chars.to_a.sort.join.downcase
+  #   end
+  # end
+end
