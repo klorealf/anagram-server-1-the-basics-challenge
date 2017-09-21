e0b85308936ad4cdebb410ab2a02bbe7b305d593
Andreas <landgrebea@allegheny.edu>
2017-09-20 22:15:52 -0400

    add logic to find anagrams with word specified in the url.

--- app/controllers/words.rb
+++ app/controllers/words.rb
@@ -1,6 +1,13 @@
 get '/words/:word' do
-  @word = params[:word]
-
+@word = params[:word]
   # Look in app/views/anagrams/index.erb
+  #@article = Word.find_by(word: @word)
+  @FoundWordsArray = []
+  Wordpermutationsarray = params[:word].chars.permutation.map &:join
+  Wordpermutationsarray.each do |permuteword|
+  	if Word.exists?(word: permuteword + "\n")
+  		@FoundWordsArray.push(permuteword)
+  	end
+  end
   erb :"words/index"
 end
